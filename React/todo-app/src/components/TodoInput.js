4c35ad0b6e431618e9ca1e1f6b2fdf1bfd10b50e
Elyelin Carrasquero <elyelin15@gmail.com>
2022-04-27 15:46:45 -0300

    todo input / card

---
+++ React/todo-app/src/components/TodoInput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/TodoInput.css'
+
+function TodoInput() {
+  return (
+    <input className='TodoInput' type="text"></input>
+  )
+}
+
+export{ TodoInput}
