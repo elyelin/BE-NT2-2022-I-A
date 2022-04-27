5640cd89df39a0a63ded1b98f7be3dd53d1eca72
Elyelin Carrasquero <elyelin15@gmail.com>
2022-04-27 15:47:29 -0300

    app prinpipal

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,33 +1,65 @@
-import './App.css';
-import NewTodo from './components/newTodo';
-import Button from './components/button'
+import React from "react";
+import "./App.css";
+import { TodoList } from "./components/TodoList";
+import { Button } from "./components/button";
+import { TodoItem } from "./components/TodoItem";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoInput } from "./components/TodoInput";
 
-function App() {
 const callbBckButton = () => {
-  console.log('F')
-}
+  console.log("F");
+};
 
-//3 estados, tareas, cantidad de tareas, teras pendientes, eventos, eventos de array 
-//por cada elemento del array renderizo el componente de todo STATE
-//chale
+const DefaultTodos = [
+  {
+    text: "Primero",
+    completed: true
+  },
+  {
+    text: "segundo",
+    completed: false
+  },
+  {
+    text: "tercero",
+    completed: true
+  },
+];
+function App() {
+  const [todos, setTodos] = React.useState(DefaultTodos)
+  const [checked, setChecked] = React.useState(false)
+  const [cantidad, setCantidad] = React.useState({
+    cantidadTodos: todos.length,
+    cantidadPendientes: ''
+  })
+  const pendingTasks = todos.filter((item) => !item.completed)
+
+  //3 estados, tareas, cantidad de tareas, teras pendientes, eventos, eventos de array
+  //por cada elemento del array renderizo el componente de todo STATE
+  const onChangeComplete = () => {
+   setChecked(!checked)
+  }
+  const onDelete = () => {
+    alert("eliminado")
+  }
   return (
-    <div className="container center">
-      <h1 className='center title'>TODO App</h1>
-
-      <div className='flow-right controls'>
-        <span>Cantidad de Todos: 3 </span>
-        <span>Todos pendientes: 3</span>
+    <React.Fragment>
+      <div className="container">
+        <TodoCounter
+        total={cantidad.cantidadTodos}
+        pendingTasks={pendingTasks.length}/>
+        <TodoInput />
+        <Button callBack={callbBckButton} />
+        <TodoList>
+          {
+            //  recorro la lista de todos y por cada elemento existente voy a renderizar un componente todoItem
+            DefaultTodos.map((todo, index) => { 
+             return <TodoItem onCompleted={onChangeComplete} onDelete={onDelete} key={todo.text} text={todo.text} completed={checked}
+              />
+            })
+          }
+        </TodoList>
       </div>
-
-<Button callBack={callbBckButton}/>
-<ul className='todo-list'>
-  <NewTodo/>
- <NewTodo/>
- <NewTodo/>
-</ul>
- 
-
-    </div>
+    </React.Fragment>
   );
 }
 
